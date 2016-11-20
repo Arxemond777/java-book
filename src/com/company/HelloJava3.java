d5e4312fdf32674132bc9c635e21c770512e2434
Юрий Глушенков <urijglusenkov@MacBook-Pro-Urij.local>
2016-11-20 22:19:08 +0300

    $date_now

---
+++ src/com/company/HelloJava3.java
@@ -0,0 +1,84 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+import java.util.Arrays;
+
+public class HelloJava3 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("HelloJava3");
+        frame.add(new HelloComponent3("Hello Java!"));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
+
+class HelloComponent3 extends JComponent
+        implements MouseMotionListener, ActionListener {
+    String theMessage;
+    int messageX = 125, messageY = 90;
+
+    JButton theButton;
+
+    int colorIndex;
+
+    final static Color customiseColor = new Color(1, 2, 3);
+    final Random random = new Random();
+
+    static Color[] someColors = {
+            Color.black, Color.red, Color.green, Color.blue,
+            Color.magenta, HelloComponent3.customiseColor
+    };
+
+    //static Color[] someColorsRandom = new ArrayList<Integer>();
+
+    public HelloComponent3(String message) {
+        //someColorsRandom.add(1);
+        //System.out.println(Arrays.toString(someColors));
+        //mInfoTextView // это массив
+        //int randomColor = mInfoTextView.setText(String.valueOf(random.nextInt(6) + 1));
+
+
+
+        theMessage = message;
+        theButton = new JButton("Change Color");
+        setLayout(new FlowLayout());
+        add(theButton);
+        theButton.addActionListener(this);
+        addMouseMotionListener(this);
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawString(theMessage, messageX, messageY);
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        messageX = e.getX();
+        messageY = e.getY();
+        repaint();
+    }
+
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == theButton) changeColor();
+
+    }
+
+    synchronized private void changeColor() {
+        if (++colorIndex == someColors.length)
+            colorIndex = 0;
+
+        setForeground(currentColor());
+        repaint();
+    }
+
+    synchronized private Color currentColor() {
+        return someColors[colorIndex];
+    }
+}
