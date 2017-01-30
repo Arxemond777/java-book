56b337eaeb693358f4e76e36df096a680e13a306
Юрий Глушенков <urijglusenkov@MacBook-Pro-Urij.local>
2017-01-30 22:33:30 +0300

    Mon Jan 30 22:33:30 MSK 2017

---
+++ src/ch11/LoggingApi.java
@@ -0,0 +1,50 @@
+package ch11;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.*;
+
+public class LoggingApi
+{
+    static Logger log = Logger.getLogger("ch11.LoggingApi");
+
+    public static void main(String[] args) throws IOException {
+        LoggingApi.a();
+        System.exit(0);
+
+        log.setLevel(Level.CONFIG);
+        Filter filter = new Filter()
+        {
+            @Override
+            public boolean isLoggable(LogRecord record) {
+                return record.getMessage().startsWith("FINER");
+            }
+        };
+        log.setFilter(filter);
+        log.warning("warning");
+        //pw.println(log.warning("warning"));
+        log.info("info");
+        Logger.global.info("123");
+
+    }
+
+    static void a() throws IOException {
+        boolean append = false;
+        FileHandler handler = new FileHandler("default.log", append);
+
+        handler.setFilter(new Filter() {
+            @Override
+            public boolean isLoggable(LogRecord record) {
+                return record.getMessage().startsWith("IMPORTANT");
+            }
+        });
+
+        Logger logger = Logger.getLogger("com.javacodegeeks.snippets.core");
+        logger.addHandler(handler);
+        logger.setLevel(Level.FINEST);
+
+        logger.info("info message");
+        logger.info("IMPORTANT info message");
+    }
+}
---
+++ src/ch12/B.java
@@ -0,0 +1,12 @@
+package ch12;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
+public class B
+{
+    public static void main(String[] args) {
+
+
+    }
+}
