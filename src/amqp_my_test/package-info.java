9f9b6b19df82da0df0d8db813fd972fee787ec4a
Юрий Глушенков <urijglusenkov@MacBook-Pro-Urij.local>
2017-03-22 22:02:38 +0300

    Wed Mar 22 22:02:38 MSK 2017

---
+++ src/amqp_my_test/package-info.java
@@ -0,0 +1,14 @@
+package amqp_my_test;
+
+   /*
+    * Management Plugin enabled by default at http://localhost:15672
+    *
+    * Bash completion has been installed to:
+    *  /usr/local/etc/bash_completion.d
+    *
+    * To have launchd start rabbitmq now and restart at login:
+    *  brew services start rabbitmq
+    * Or, if you don't want/need a background service you can just run:
+    *  rabbitmq-server
+    *
+    * */
