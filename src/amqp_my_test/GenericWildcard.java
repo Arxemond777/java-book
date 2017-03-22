d35c9b9556e4d44ddcd5ae8690e2c6efcca86576
Юрий Глушенков <urijglusenkov@MacBook-Pro-Urij.local>
2017-03-22 23:49:50 +0300

    Wed Mar 22 23:49:50 MSK 2017

--- src/amqp_my_test/GenericWildcard.java
+++ src/amqp_my_test/GenericWildcard.java
@@ -2,6 +2,8 @@ package amqp_my_test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class GenericWildcard
 {
@@ -10,6 +12,36 @@ public class GenericWildcard
         GenericWildcard.Ggg b = new GenericWildcard().new Ggg();
 //        a.add(b);
 //        a.add(new Integer(1)); //Вообще сетерить незя, тк ? означает неопределенный тип
+
+        /*Comparator<Integer> comparator = new Comparator<Integer>() {
+
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if( o1 > o2 ){
+                    return -1;
+                }
+                if( o1 < o2 ){
+                    return 1;
+                }
+                return 0;
+            }
+        };*/
+
+        Queue<Integer> intQueue = new PriorityQueue<>(10, (Integer o1, Integer o2) -> {
+            if( o1 > o2 ){
+                return -1;
+            }
+            if( o1 < o2 ){
+                return 1;
+            }
+            return 0;
+        });
+        intQueue.add(1);
+        intQueue.add(12);
+        intQueue.add(8);
+        while( !intQueue.isEmpty() ){
+            System.out.println( intQueue.remove() );
+        }
     }
 
     class Ggg {
