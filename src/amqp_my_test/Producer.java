13099d6b144224a505b5bd92ee3dfac8bfced73e
Yuri Glushenkov <glushenkov@rg.ru>
2017-03-24 10:52:01 +0300

    Пт мар 24 10:52:01 MSK 2017

--- src/amqp_my_test/Producer.java
+++ src/amqp_my_test/Producer.java
@@ -22,8 +22,9 @@ public class Producer
         Channel channel = connection.createChannel();
 
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        String message = "Hello World! date" + new Date().toString();
+        String message = "Hello World! date: " + new Date().toString();
         channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        channel.basicPublish("", QUEUE_NAME, null, "xer".getBytes("UTF-8"));
         System.out.println(" [x] Sent '" + message + "'");
 
         channel.close();
