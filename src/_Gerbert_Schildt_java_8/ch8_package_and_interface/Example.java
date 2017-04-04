fa82d42ea911247224dced012f4617f0c804d03a
Юрий Глушенков <urijglusenkov@MacBook-Pro-Urij.local>
2017-04-04 23:20:44 +0300

    Tue Apr  4 23:20:44 MSK 2017

---
+++ src/_Gerbert_Schildt_java_8/ch8_package_and_interface/Example.java
@@ -0,0 +1,42 @@
+package _Gerbert_Schildt_java_8.ch8_package_and_interface;
+
+import com.sun.istack.internal.NotNull;
+
+public class Example
+{
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+        Abc abc = Abc.class.newInstance();
+        System.out.println(
+                "Default method: " + abc.abc() + System.lineSeparator()
+                + "Static interface% " + Default.def() + System.lineSeparator()
+                + " | " + abc.i()
+        );
+    }
+}
+
+interface Default
+{
+    default double abc() {
+        return Math.random();
+    }
+
+    static int def() {
+        return 666;
+    }
+}
+
+class Abc implements Default
+{
+    @NotNull
+    protected int i() {
+        return Integer.parseInt(null);
+    }
+}
+
+class Test
+{
+    void u() {
+        Abc abc = new Abc();
+        abc.i();
+    }
+}
